7fa33fa93e2383afca81caa7b78c9e6869a23451
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-04-16 13:45:55 -0400

    fixed style of chat

--- components/Chat.js
+++ components/Chat.js
@@ -14,7 +14,7 @@ export default class Screen2 extends React.Component {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: {this.state.color}}}>
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Hello, welcome to the View</Text>
       </View>
     )
