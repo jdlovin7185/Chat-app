71c153837fea1447c6fb3478bfb332e0444aaeff
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-04-22 20:52:43 -0400

    added firebase db for user auth

--- components/Chat.js
+++ components/Chat.js
@@ -2,74 +2,117 @@ import React from 'react';
 import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
 export default class Screen2 extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: '',
-      color: '',
       messages: []
-    }
     };
-
+    if (!firebase.apps.length){
+      firebase.initializeApp({
+        apiKey: "AIzaSyAhsnNVYW1Q7SLxej_YotqhWZw1jwtCuSY",
+        authDomain: "meet-app-da2c9.firebaseapp.com",
+        projectId: "meet-app-da2c9",
+        storageBucket: "meet-app-da2c9.appspot.com",
+        messagingSenderId: "316391670730",
+        appId: "1:316391670730:web:f1a0c326742b3a421bc533",
+        measurementId: "G-RTQF5PMVMG"
+      });
+    }
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+  };
+  
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: 'This is a system message',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+    
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async(user) => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+      
+      // update user state with currently active user data
+      this.setState({
+        uid: user.uid,
+        messages: [],
+      });
+      this.unsubscribe = this.referenceChatMessages
+      .orderBy("createdAt", "desc")
+      .onSnapshot(this.onCollectionUpdate);
     });
+    
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  // create a reference to the active user's documents ()
+  onAuthStateChanged() {
+    this.referenceChatMessagesUser = 
+    firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
   }
 
+  
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        createdAt: data.createdAt.toDate(), 
+        text: data.text,
+        user: data.user
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
+  
+  addMessages() {
+    this.referenceChatMessages.add({
+      _id: messages._id,
+      text: messages.text,
+      createdAt: messages.createdAt,
+      user: message.user
+    });
+  }
+  
   // Keeps messages in the chat 
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
     }))
   }
+  
 
   // Styles the chat bubble on the right side
   renderBubble(props) {
     return (
       <Bubble 
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: '#000'
-          }
-        }}
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#000'
+        }
+      }}
       />
-    )
+      )
   }
 
   render() {
-    let name = this.props.route.params.name;
-    let color = this.props.route.params.color;
-    this.props.navigation.setOptions({ title: name, backgroundColor: color });
     return (
-      <View style={{ backgroundColor: color }}>
+      <View style={{ flex: 1 }}>
          <GiftedChat 
-         style={{ backgroundColor: color }}
          renderBubble={this.renderBubble.bind(this)}
             messages={this.state.messages}
             onSend={messages => this.onSend(messages)}
-            user={{
-              _id: 1,
-            }}
+            user={ {_id: 1}
+            }
           />
       </View>
     )
